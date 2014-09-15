ccce757cb3c0d2414e4df6b87a91a32cc69148ea
Geoffrey Hervet <hervetg@gmail.com>
2014-09-15 23:53:36 +0200

    Refactoring code

---
+++ NancyFxApp/Helpers/NotFoundResponse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nancy;
+using Nancy.Responses.Negotiation;
+
+namespace NancyFxApp.Helpers
+{
+    public class NotFoundResponse
+    {
+        public static Negotiator NotFoundWithId(Negotiator negociator, int id)
+        {
+            return negociator
+                .WithStatusCode(HttpStatusCode.NotFound)
+                .WithModel(String.Format("Item with id {0} not found.", id))
+            ;
+        }
+    }
+}
--- NancyFxApp/Source/SourceModule.cs
+++ NancyFxApp/Source/SourceModule.cs
@@ -4,63 +4,101 @@ using Nancy;
 using Nancy.ModelBinding;
 using NancyFxApp.Repository;
 using Nancy.Validation;
+using Nancy.Responses.Negotiation;
 
 namespace NancyFxApp.Source
 {
     public class SourceModule : NancyModule
     {
+        enum Action { CREATE, READ, UPDATE, DELETE};
+        protected Repository.Repository _repository;
         public SourceModule()
             : base("/sources")
         {
             StaticConfiguration.DisableErrorTraces = false;
-            var repository = Repository.Repository.getInstance();
+            _repository = Repository.Repository.getInstance();
 
             Get["/list/{page?:int)"] = parameters =>
             {
-                int page = parameters.page;
-                page = Math.Max(page != null ? page : 0, 1);
+                int page = Math.Max(parameters.page, 1);
 
                 return Negotiate
                     .WithStatusCode(HttpStatusCode.OK)
-                    .WithModel(repository.findAll<Source>(page));
+                    .WithModel(_repository.findAll<Source>(page));
             };
 
             Get["/validators"] = parameters =>
             {
-                var validator =
-                    this.ValidatorLocator.GetValidatorForType(typeof(Source));
+                var validator =  this.ValidatorLocator.GetValidatorForType(typeof(Source));
 
                 return this.Response.AsJson(validator.Description);
             };
 
             Post["/"] = _ =>
             {
-                Source model = this.Bind();
-                var result = this.Validate(model);
-
-                if (!result.IsValid)
-                {
-                    return Negotiate
-                        .WithStatusCode(HttpStatusCode.UnprocessableEntity)
-                        .WithModel(result);
-                }
-
-                repository.insert(model);
-
-                return Negotiate
-                    .WithStatusCode(HttpStatusCode.OK)
-                    .WithModel(model);
+                return processEntity(0, Action.CREATE);
             };
 
             Put["/{id:int}"] = parameters =>
             {
-                int id = parameters.id;
-                var model = repository.find<Source>(id);
+                return processEntity(parameters.id, Action.UPDATE);
+            };
+
+            Get["/{id:int}"] = parameters =>
+            {
+                return processEntity(parameters.id, Action.READ);
+            };
+
+            Delete["/{id:int}"] = parameters =>
+            {
+                return processEntity(parameters.id, Action.DELETE);
+            };
+        }
+
+        protected Source _find(int id)
+        {
+            return _repository.find<Source>(id);
+        }
+
+        protected Negotiator _reponseModel(Source model)
+        {
+            return Negotiate
+                .WithStatusCode(HttpStatusCode.OK)
+                .WithModel(model);
+        }
+
+        protected Negotiator _notFoundWithId(int id)
+        {
+            return Negotiate
+                .WithStatusCode(HttpStatusCode.NotFound)
+                .WithModel(String.Format("Item with id {0} not found.", id))
+            ;
+        }
+
+        private Negotiator processEntity(int id, Action action)
+        {
+            Source model = null;
+
+            if (action == Action.CREATE)
+            {
+                model = this.Bind();
+            }
+            else
+            {
+                model = _find(id);
                 if (model == null)
                 {
-                    return Negotiate.WithStatusCode(HttpStatusCode.NotFound).WithModel(String.Format("Item with id {0} not found.", id));
+                    return _notFoundWithId(id);
                 }
+            }
 
+            if (action == Action.UPDATE)
+            {
+                model = this.BindTo(model);
+            }
+
+            if (action == Action.UPDATE || action == Action.CREATE)
+            {
                 model = this.BindTo(model);
                 var result = this.Validate(model);
 
@@ -71,41 +109,21 @@ namespace NancyFxApp.Source
                         .WithModel(result);
                 }
 
-                repository.update(model);
-
-                return Negotiate
-                    .WithStatusCode(HttpStatusCode.OK)
-                    .WithModel(model);
-            };
-
-            Get["/{id:int}"] = parameters =>
-            {
-                int id = parameters.id;
-                var model = repository.find<Source>(id);
-                if (model == null)
+                if (action == Action.UPDATE)
                 {
-                    return Negotiate.WithStatusCode(HttpStatusCode.NotFound).WithModel(String.Format("Item with id {0} not found.", id));
+                    _repository.update(model);
                 }
-
-                return Negotiate
-                    .WithStatusCode(HttpStatusCode.OK)
-                    .WithModel(model);
-            };
-
-            Delete["/{id:int}"] = parameters =>
-            {
-                int id = parameters.id;
-                var model = repository.find<Source>(id);
-                if (model == null)
+                else if (action == Action.CREATE)
                 {
-                    return Negotiate.WithStatusCode(HttpStatusCode.NotFound).WithModel(String.Format("Item with id {0} not found.", id));
+                    _repository.insert(model);
                 }
+            }
+            else if (action == Action.DELETE)
+            {
+                _repository.delete<Source>(id);
+            }
 
-                repository.delete<Source>(id);
-                return Negotiate
-                    .WithStatusCode(HttpStatusCode.OK)
-                    .WithModel(model);
-            };
+            return _reponseModel(model);
         }
     }
 }
