bbaedcebddb7532b9c0990cb012dce52dc87122a
Geoffrey Hervet <hervetg@gmail.com>
2014-09-16 21:46:47 +0200

    Refactoring

---
+++ NancyFxApp/Generics/Module.cs
@@ -0,0 +1,155 @@
+﻿namespace NancyFxApp.Generics
+{
+    using System;
+    using Nancy;
+    using Nancy.Responses.Negotiation;
+    using Nancy.ModelBinding;
+    using Nancy.Validation;
+    public enum Action { CREATE, READ, UPDATE, DELETE, LIST, VALIDATORS };
+    public abstract class Module<T> : NancyModule
+    {
+
+        protected Repository.Repository _repository;
+
+        public Module(String baseUri)
+            : base(baseUri)
+        {
+            StaticConfiguration.DisableErrorTraces = false;
+            _repository = Repository.Repository.getInstance();
+
+
+            if (!isDisabledAction(Action.LIST))
+            {
+                Get["/list/{page?:int)"] = parameters =>
+                {
+                    int page = Math.Max(parameters.page, 1);
+
+                    return Negotiate
+                        .WithStatusCode(HttpStatusCode.OK)
+                        .WithModel(_repository.findAll<T>(page));
+                };
+            }
+
+            if (!isDisabledAction(Action.VALIDATORS))
+            {
+                Get["/validators"] = parameters =>
+                {
+                    var validator = this.ValidatorLocator.GetValidatorForType(typeof(T));
+
+                    return this.Response.AsJson(validator.Description);
+                };
+            }
+
+
+            if (!isDisabledAction(Action.CREATE))
+            {
+                Post["/"] = _ =>
+                {
+                    return processEntity(0, Action.CREATE);
+                };
+            }
+
+            if (!isDisabledAction(Action.UPDATE))
+            {
+                Put["/{id:int}"] = parameters =>
+                {
+                    return processEntity(parameters.id, Action.UPDATE);
+                };
+            }
+
+            if (!isDisabledAction(Action.READ))
+            {
+                Get["/{id:int}"] = parameters =>
+                {
+                    return processEntity(parameters.id, Action.READ);
+                };
+            }
+
+            if (!isDisabledAction(Action.DELETE))
+            {
+                Delete["/{id:int}"] = parameters =>
+                {
+                    return processEntity(parameters.id, Action.DELETE);
+                };
+            }
+
+        }
+
+        public virtual bool isDisabledAction(Action a)
+        {
+            return false;
+        }
+
+        protected T _find(int id)
+        {
+            return _repository.find<T>(id);
+        }
+
+        protected Negotiator _reponseModel(T model)
+        {
+            return Negotiate
+                .WithStatusCode(HttpStatusCode.OK)
+                .WithModel(model);
+        }
+
+        protected Negotiator _notFoundWithId(int id)
+        {
+            return Negotiate
+                .WithStatusCode(HttpStatusCode.NotFound)
+                .WithModel(String.Format("Item with id {0} not found.", id))
+            ;
+        }
+
+        private Negotiator processEntity(int id, Action action)
+        {
+            T model;
+
+            if (action == Action.CREATE)
+            {
+                model = this.Bind();
+            }
+            else
+            {
+                model = _find(id);
+                if (model == null)
+                {
+                    return _notFoundWithId(id);
+                }
+            }
+
+            if (action == Action.UPDATE)
+            {
+                model = this.BindTo(model);
+            }
+
+            if (action == Action.UPDATE || action == Action.CREATE)
+            {
+                model = this.BindTo(model);
+                var result = this.Validate(model);
+
+                if (!result.IsValid)
+                {
+                    return Negotiate
+                        .WithStatusCode(HttpStatusCode.UnprocessableEntity)
+                        .WithModel(result);
+                }
+
+                if (action == Action.UPDATE)
+                {
+                    _repository.update(model);
+                }
+                else if (action == Action.CREATE)
+                {
+                    _repository.insert(model);
+                }
+            }
+            else if (action == Action.DELETE)
+            {
+                _repository.delete<T>(id);
+            }
+
+            return _reponseModel(model);
+        }
+
+    }
+}
--- NancyFxApp/Source/SourceModule.cs
+++ NancyFxApp/Source/SourceModule.cs
@@ -1,129 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using Nancy;
-using Nancy.ModelBinding;
-using NancyFxApp.Repository;
-using Nancy.Validation;
-using Nancy.Responses.Negotiation;
-
-namespace NancyFxApp.Source
+﻿namespace NancyFxApp.Source
 {
-    public class SourceModule : NancyModule
+    public class SourceModule : Generics.Module<Source>
     {
-        enum Action { CREATE, READ, UPDATE, DELETE};
-        protected Repository.Repository _repository;
-        public SourceModule()
-            : base("/sources")
+        public SourceModule() : base("/sources")
         {
-            StaticConfiguration.DisableErrorTraces = false;
-            _repository = Repository.Repository.getInstance();
-
-            Get["/list/{page?:int)"] = parameters =>
-            {
-                int page = Math.Max(parameters.page, 1);
-
-                return Negotiate
-                    .WithStatusCode(HttpStatusCode.OK)
-                    .WithModel(_repository.findAll<Source>(page));
-            };
-
-            Get["/validators"] = parameters =>
-            {
-                var validator =  this.ValidatorLocator.GetValidatorForType(typeof(Source));
-
-                return this.Response.AsJson(validator.Description);
-            };
-
-            Post["/"] = _ =>
-            {
-                return processEntity(0, Action.CREATE);
-            };
-
-            Put["/{id:int}"] = parameters =>
-            {
-                return processEntity(parameters.id, Action.UPDATE);
-            };
-
-            Get["/{id:int}"] = parameters =>
-            {
-                return processEntity(parameters.id, Action.READ);
-            };
-
-            Delete["/{id:int}"] = parameters =>
-            {
-                return processEntity(parameters.id, Action.DELETE);
-            };
         }
 
-        protected Source _find(int id)
+        public override bool isDisabledAction(Generics.Action a)
         {
-            return _repository.find<Source>(id);
+            return (a == Generics.Action.READ);
         }
 
-        protected Negotiator _reponseModel(Source model)
-        {
-            return Negotiate
-                .WithStatusCode(HttpStatusCode.OK)
-                .WithModel(model);
-        }
-
-        protected Negotiator _notFoundWithId(int id)
-        {
-            return Negotiate
-                .WithStatusCode(HttpStatusCode.NotFound)
-                .WithModel(String.Format("Item with id {0} not found.", id))
-            ;
-        }
-
-        private Negotiator processEntity(int id, Action action)
-        {
-            Source model = null;
-
-            if (action == Action.CREATE)
-            {
-                model = this.Bind();
-            }
-            else
-            {
-                model = _find(id);
-                if (model == null)
-                {
-                    return _notFoundWithId(id);
-                }
-            }
-
-            if (action == Action.UPDATE)
-            {
-                model = this.BindTo(model);
-            }
-
-            if (action == Action.UPDATE || action == Action.CREATE)
-            {
-                model = this.BindTo(model);
-                var result = this.Validate(model);
-
-                if (!result.IsValid)
-                {
-                    return Negotiate
-                        .WithStatusCode(HttpStatusCode.UnprocessableEntity)
-                        .WithModel(result);
-                }
-
-                if (action == Action.UPDATE)
-                {
-                    _repository.update(model);
-                }
-                else if (action == Action.CREATE)
-                {
-                    _repository.insert(model);
-                }
-            }
-            else if (action == Action.DELETE)
-            {
-                _repository.delete<Source>(id);
-            }
-
-            return _reponseModel(model);
-        }
     }
 }
