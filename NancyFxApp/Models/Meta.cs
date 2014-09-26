355868ffd1a24693419e743dfcddff123159cf99
Geoffrey Hervet <hervetg@gmail.com>
2014-09-26 20:53:25 +0200

    Model Meta

---
+++ NancyFxApp/Models/Meta.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace NancyFxApp.Models
+{
+    
+    [PetaPoco.TableName("source_meta")]
+    [PetaPoco.PrimaryKey("Id")]
+    [PetaPoco.ExplicitColumns]
+    public class Meta
+    {
+        [PetaPoco.Column("id")]
+        public int Id { get; set; }
+        
+        [PetaPoco.Column("created_at")]
+        public DateTime CreatedAt { get; set; }
+        
+        [PetaPoco.Column("updated_at")]
+        public DateTime UpdatedAt { get; set; }
+  
+        [Required, StringLength(255)]
+        [PetaPoco.Column("name")]
+        public String Name { get; set; }
+    }
+}
